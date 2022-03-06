2598c6af7b17680747adb198e22ec337ba08cb52
MlMauriciolopes <mlsilva1990@gmail.com>
2022-03-06 11:34:41 -0300

    Add files via upload

---
+++ lotus_turtle.py
@@ -0,0 +1,15 @@
+# Lotus turtle
+# @programing.network - Instagram
+
+import turtle
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black') #background
+t.pencolor('pink') #lines
+t.speed(0)
+
+for i in range(150):
+    t.circle(190-1,90)
+    t.lt(98)
+    t.circle(190-1,90)
+    t.lt(18)
